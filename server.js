75a1965c0090bb06aa930f8f5864052718982127
Christian Torres <cht1204@hotmail.com>
2017-01-18 00:49:58 -0500

    fixed dynamic port, added script in package.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT;
 
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -50,4 +51,4 @@ app.get('/projects', (req, res) => {
 	})
 })
 
-app.listen(3000);
+app.listen(port);
